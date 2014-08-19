22280e5f4e8f2f0c3633dd0fa8f218ba13c6ff97
User 2 <User 2@Lenovo-PC>
2014-08-19 15:09:51 +0530

    changed syso

--- exp.java
+++ exp.java
@@ -3,7 +3,7 @@ class exp
   public static void main(String ar[])
   {
      System.out.println("HELLO WORLD");
-     System.out.println("my name is veeralakshmi");
+     System.out.println("my name is veer alakshmi");
      sysout
   }
 }
